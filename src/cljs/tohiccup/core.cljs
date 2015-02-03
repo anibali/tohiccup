e23d1b5e1a972eee010d2d60712687072b58e37c
Aiden Nibali <dismaldenizen@gmail.com>
2015-02-03 21:24:54 +1100

    Added components for text areas

--- src/cljs/tohiccup/core.cljs
+++ src/cljs/tohiccup/core.cljs
@@ -11,6 +11,24 @@
 
 (defn process-action [_])
 
+(defcomponentk html-input-view
+  "HTML text input view"
+  [data owner]
+  (display-name [this] "HtmlArea")
+  (render-state [this {:keys [action-chan new-item-title]}]
+    (html [:div
+           [:h2 "<html>"]
+           [:textarea {:class "form-control"}]])))
+
+(defcomponentk hiccup-input-view
+  "Hiccup text input view"
+  [data owner]
+  (display-name [this] "HiccupArea")
+  (render-state [this {:keys [action-chan new-item-title]}]
+    (html [:div
+           [:h2 "[:hiccup]"]
+           [:textarea {:class "form-control"}]])))
+
 (defcomponentk converter-view
   "Top-level HTML <-> Hiccup converter component"
   [data owner]
@@ -23,7 +41,13 @@
                (process-action (<! action-chan))
                (recur))))
   (render-state [this {:keys [action-chan new-item-title]}]
-    (html [:h2 "Under construction"])))
+    (html [:div {:class "row"}
+           [:div {:class "col-sm-6"}
+            (om/build html-input-view data
+                      {:init-state {:action-chan action-chan}})]
+           [:div {:class "col-sm-6"}
+            (om/build hiccup-input-view data
+                      {:init-state {:action-chan action-chan}})]])))
 
 (defn main []
   (om/root converter-view app-state
