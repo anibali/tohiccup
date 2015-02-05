1c806b4c460e233a2f9d35cf85340b2caf62d51e
Aiden Nibali <dismaldenizen@gmail.com>
2015-02-05 22:48:58 +1100

    Removed function name clobbering

--- project.clj
+++ project.clj
@@ -59,5 +59,5 @@
                        :cljsbuild {:builds {:app
                                             {:source-paths ["env/prod/cljs"]
                                              :compiler
-                                             {:optimizations :advanced
+                                             {:optimizations :simple
                                               :pretty-print false}}}}}})
